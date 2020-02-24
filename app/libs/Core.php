9cc8acfda0199b4d5566faa4e453abd48ddea3a0
andereric <37287866+andereric@users.noreply.github.com>
2020-02-24 22:14:46 +0200

    Loodud url andmete lugemine ja teisendamine

--- app/libs/Core.php
+++ app/libs/Core.php
@@ -1,7 +1,31 @@
 <?php
-
+/*
+ * http://hostname/veeb/kontroller/meetod/parameeter(id)
+ * $_GET['url'] = 'kontroller/meetod/parameeter(id)'
+ * http://hostname/veeb/pages/show/19/02/2020
+ */
 
 class Core
 {
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = array();
 
+    public function __construct()
+    {
+        $url = $this->getUrl();
+        echo '<pre>';
+        print_r($url);
+        echo '</pre>';
+    }
+
+    // get url from link and prepare for use
+    public function getUrl(){
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url,FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
 }
