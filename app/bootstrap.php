3ffadf09b2ceb1de3562150916897d331409a841
andereric <37287866+andereric@users.noreply.github.com>
2020-03-04 11:00:26 +0200

    Täiendatud kasutaja suunamine erinevatele lehtelede

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
 error_reporting(~E_NOTICE & ~E_WARNING);
+// config
 require_once 'config/config.php';
-
+// helpers files
+require_once 'helpers/url_helper.php';
+// system
 require_once 'libs/Core.php';
 require_once 'libs/Controller.php';
 require_once 'libs/Database.php';
