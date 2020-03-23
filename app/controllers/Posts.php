6d10e126afb42aa8580278a06e213c07647ffb86
andereric <37287866+andereric@users.noreply.github.com>
2020-03-23 16:38:01 +0200

    Loodud blogi postituste näitamine pealehel

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -3,7 +3,14 @@
 
 class Posts extends Controller
 {
+    public function __construct(){
+        $this->postModel = $this->model('Post');
+    }
     public function index(){
+        if($this->postModel->getAllPosts() !== false){
+            $data = $this->postModel->getAllPosts();
+            $this->view('posts/index', $data);
+        }
         $this->view('posts/index');
     }
 }
--- app/models/Post.php
+++ app/models/Post.php
@@ -3,5 +3,26 @@
 
 class Post
 {
+    private $db;
 
+    public function __construct(){
+        $this->db = new Database();
+    }
+
+    public function getAllPosts(){
+        $this->db->query('SELECT *,
+        posts.id as postID,
+        users.id as userID,
+        users.created_at as userCreated
+        FROM posts
+        INNER JOIN users
+        ON posts.user_id = users.id
+        ORDER BY posts.created_at DESC');
+        $posts = $this->db->getAll();
+        if($this->db->rowCount() > 0){
+            return $posts;
+        } else {
+            return false;
+        }
+    }
 }
