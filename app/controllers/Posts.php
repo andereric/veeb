fc21186ec9b94ade8a3a56d6ecfb48b0f39f8cc4
andereric <37287866+andereric@users.noreply.github.com>
2020-03-23 16:53:51 +0200

    Loodud blogi üksiku postituste näitamine

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -5,6 +5,7 @@ class Posts extends Controller
 {
     public function __construct(){
         $this->postModel = $this->model('Post');
+        $this->userModel = $this->model('User');
     }
     public function index(){
         if($this->postModel->getAllPosts() !== false){
@@ -12,6 +13,15 @@ class Posts extends Controller
             $this->view('posts/index', $data);
         }
         $this->view('posts/index');
+    }
+    public function show($id){
+        $post = $this->postModel->getPostById($id);
+        $user = $this->userModel->getUserById($post->user_id);
+        $data = array(
+            'post' => $post,
+            'user' => $user
+        );
+        $this->view('posts/show', $data);
     }
 }
 
